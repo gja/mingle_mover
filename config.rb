f5432cd10d818cbdd7f1ac03353ec017f17b1a22
Tejas Dinkar <tejas@gja.in>
2010-05-04 11:22:06 +0530

    Added open to ugly

--- config.rb
+++ config.rb
@@ -13,7 +13,7 @@ $columns = [CardColumn.new("number", "Number"),
 
 $good_status = ["QA Closed", "Ready for QA", "In QA", "Available in UAT", "Ready for Acceptance", "Accepted"]
 $bad_status = ["In Dev", "In Development"]
-$ugly_status = ["Ready for Development"]
+$ugly_status = ["Open", "Ready for Development"]
 
 begin
     require File.dirname(__FILE__) + '/actual_config'
